df07297dd4d66447a04479f249a061a9f4acbb73
tienld <ldtien.tr@gmail.com>
2024-10-23 15:09:37 +0700

    add create pdf

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "441322401"
+    serviceWorkerVersion: "2459566113"
   }
 });
